91d8d258cfda50b70b7437cb6ab5c908133210c9
Cem Karaca <cem.karaca@gmail.com>
2022-06-25 08:37:12 +0300

    bookmark test

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 
-
-
+const element = <h1>Hello, world!</h1>;
+console.log(element);
 
 function App() {
   return <h1>Hello World!</h1>;
